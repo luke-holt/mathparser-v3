bc5a25afe1689797ee3705f9d2b9eece799b1bea
Luke Holt <luke@lukeholt.dev>
2024-05-01 18:44:05 -0400

    fix help msg

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int
 main(int argc, char *argv[])
 {
     if (argc != 2)
-        die("usage: %s <expr>\nsupported operations: [ +, -, *, /, ** ]");
+        die("usage: %s <expr>\nsupported operations: [ +, -, *, /, ** ]", argv[0]);
 
     mathparser_eval_expr(argv[1]);
 
